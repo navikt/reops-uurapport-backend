83478e792d992d2dd7f4a92c77b1e3cbf47a312c
Kenneth Isaksen <kenneth.bakke.isaksen@nav.no>
2025-12-15 14:15:07 +0100

    feat: fix url

--- src/main/kotlin/accessibility/reporting/tool/html/landingPageRoute.kt
+++ src/main/kotlin/accessibility/reporting/tool/html/landingPageRoute.kt
@@ -55,8 +55,8 @@ object NewPage {
     val url = resolveNewPageUrl()
     private fun resolveNewPageUrl(): String =
         when (System.getenv("NAIS_CLUSTER_NAME")) {
-            "prod-gcp" -> "https://reops-a11y-statement-ny.ansatt.nav.no/"
-            "dev-gcp" -> "https://reops-a11y-statement.ansatt.dev.nav.no/"
+            "prod-gcp" -> "https://reops-a11y-statement.ansatt.nav.no/" // todo: should not be hardcoded
+            "dev-gcp" -> "https://reops-a11y-statement.ansatt.dev.nav.no/" // todo: should not be hardcoded
             else -> "https://reops-a11y-statement.ansatt.dev.nav.no/"
         }
 }
